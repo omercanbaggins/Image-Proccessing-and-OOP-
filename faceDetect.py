a1f398467f6f73f80b8ff0c2f1ebaa3f78d2294d
Ömer Can Demirci <68750887+omercanbaggins@users.noreply.github.com>
2026-02-10 01:20:00 +0300

    Add files via upload
    If the algorithm detects a face on the image shown by the user, it will calculate the angle between the center point of the image and the rectangle which represents the face.

---
+++ faceDetect.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+print("hello face Detection")
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+def findVectorLenght(vect1):
+    x,y = vect1
+    length = np.sqrt(np.pow(x,2)+np.pow(y,2))
+    return  length
+def findAngle(pt1,pt2):
+    x,y = pt1
+    x2,y2 =pt2
+    multOfLengths = findVectorLenght(pt1)*findVectorLenght(pt2)
+    cos = ((x*x2)+(y*y2))/multOfLengths
+    degree= np.acos(cos)*180/np.pi
+    return degree
+
+b = 1
+img = cv2.imread("1.jpg")
+img = cv2.resize(img,(640,480))
+while(b == 1):
+    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.1,5)
+    if len(faces)>0:
+
+        for i in faces:
+            x,y,width,height = i
+            cv2.rectangle(img,(x,y),(x+width,y+height),(255,255,255),16)
+            centerRect = (x+width//2),(y+height//2)
+            imgCenter = img.shape[0]//2,img.shape[1]//2
+            print(findAngle(imgCenter,centerRect))
+
+            cv2.line(img,imgCenter,centerRect,(50,50,50),12)
+        cv2.imshow("normalImage",img)
+    cv2.imshow("",img)
+    b = cv2.waitKey(0)
+cv2.destroyAllWindows()
+    
